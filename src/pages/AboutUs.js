730cfd21b15bcdfc285739db186577f9249cf293
Aoi <ht.so.ik0102@gmail.com>
2021-02-06 23:11:33 +0700

    3. services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 //page components
 import AboutSection from '../component/AboutSection';
+import ServiceSection from "../component/ServiceSection";
 
 const AboutUs = () => {
     return (
-        <div>
+        <>
             <AboutSection />
-        </div>
+            <ServiceSection />
+        </>
     );
 }
 
