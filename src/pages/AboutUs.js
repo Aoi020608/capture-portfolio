1a80386a32ed68f1843faa0ef266a185fa11e0aa
Aoi <ht.so.ik0102@gmail.com>
2021-02-06 22:53:42 +0700

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+//page components
+import AboutSection from '../component/AboutSection';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutSection />
+        </div>
+    );
+}
+
+export default AboutUs;
