c5417a470734e79f9e4923879b79860873e2ffcc
Aoi <ht.so.ik0102@gmail.com>
2021-02-07 09:39:59 +0700

    4. FaqSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //page components
 import AboutSection from '../component/AboutSection';
 import ServiceSection from "../component/ServiceSection";
+import FaqSection from '../component/FaqSection';
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection />
             <ServiceSection />
+            <FaqSection />
         </>
     );
 }
